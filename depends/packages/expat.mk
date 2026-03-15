babf57f471cda2450b7bef2b43d2e340989a01bb
MattF42 <matt_foster@me.com>
2026-03-15 20:13:06 +0000

    Fix SHA256 hash for expat package version 2.6.2
    Updated the SHA256 hash for the expat package.

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -2,7 +2,7 @@ package=expat
 $(package)_version=2.6.2
 $(package)_download_path=hhttps://github.com/libexpat/libexpat/releases/download/R_2_6_2/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=461ecc8aa98ab1a68c2db788175665d1a4db640dc05bf0e289b6ea17122144ec
+$(package)_sha256_hash=d4cf38d26e21a56654ffe4acd9cd5481164619626802328506a2869afab29ab3
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-static
