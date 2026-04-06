2583d2ec959fda4d13d42d86ad4d8136d61c5cb6
MattF42 <matt_foster@me.com>
2026-04-06 08:40:58 +0100

    Need XelisV2 boundary check to be present to allow bootstrap from genesis

--- src/pow.cpp
+++ src/pow.cpp
@@ -182,9 +182,10 @@ unsigned int GetNextWorkRequiredBTC(const CBlockIndex* pindexLast, const CBlockH
 
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
-    // if((pindexLast->nHeight+1) == params.nNewHashHeight) return params.nNewHashBits; // Prevent a silly stall at hash algo change 
-    // No longer needed since we are way past XelisV2 HF
+    if((pindexLast->nHeight+1) == params.nNewHashHeight) return params.nNewHashBits; // Prevent a silly stall at hash algo change 
+    // Always needed to allow full bootstrap from genesis!!!!
 
+	
 	// Regtest: make mining trivial
      if (params.fPowNoRetargeting && params.fPowAllowMinDifficultyBlocks) {
 	             return 0x207fffff;
